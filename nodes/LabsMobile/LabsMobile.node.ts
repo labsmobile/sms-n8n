4491bdea34fd920a2bceb0f42cd1b24b0e3d8135
Oscar Santa <oscar.docu@gmail.com>
2022-10-05 13:02:46 -0500

    Actualizacion node LabsMobile

--- nodes/LabsMobile/LabsMobile.node.ts
+++ nodes/LabsMobile/LabsMobile.node.ts
@@ -64,9 +64,31 @@ export class LabsMobile implements INodeType {
 						description: 'Send SMS',
 						action: 'Send SMS',
 					},
+					{
+						name: 'Scheduled Send',
+						value: 'scheduled',
+						description: 'Scheduled Send SMS',
+						action: 'Send SMS scheduled',
+					},
 				],
 				default: 'send',
 			},
+
+			{
+				displayName: 'Date Of Shipment',
+				name: 'date',
+				type: 'dateTime',
+				required: true,
+				default: '',
+				description: 'The date and time to send sms',
+				displayOptions: {
+					show: {
+						operation: ['scheduled'],
+						resource: ['sms'],
+					}
+				},
+			},
+
 			{
 				displayName: 'To',
 				name: 'to',
@@ -76,7 +98,7 @@ export class LabsMobile implements INodeType {
 				required: true,
 				displayOptions: {
 					show: {
-						operation: ['send'],
+						operation: ['send','scheduled'],
 						resource: ['sms'],
 					},
 				},
@@ -88,9 +110,12 @@ export class LabsMobile implements INodeType {
 				type: 'string',
 				default: '',
 				required: true,
+				typeOptions: {
+					rows: 4,
+				},
 				displayOptions: {
 					show: {
-						operation: ['send'],
+						operation: ['send','scheduled'],
 						resource: ['sms'],
 					},
 				},
@@ -124,6 +149,42 @@ export class LabsMobile implements INodeType {
 										"msisdn": `${to}`,
 									},
 							 ],
+					};
+					Object.assign(data);
+					const options: OptionsWithUri = {
+						method: 'POST',
+						uri: `https://api.labsmobile.com/json/send`,
+						headers: {
+							'Content-Type': 'application/json',
+							 Authorization: 'Basic ' + Buffer.from(`${credentials.username}`+':'+`${credentials.password}`).toString('base64'),
+							'Cache-Control': 'no-cache',
+						},
+						body: data,
+						json: true,
+					};
+					responseData = await this.helpers.request(options);
+					returnData.push(responseData);
+				}
+
+				if (operation === 'scheduled') {
+					const date = this.getNodeParameter('date', i) as string;
+					const to = this.getNodeParameter('to', i) as string;
+					const message = this.getNodeParameter('message', i) as string;
+
+					const dateCanedaF = date.slice(0,-14);
+					const dateCadenaH = date.slice(11,-5);
+
+					const dateCadenaResult = dateCanedaF + " " + dateCadenaH;
+
+					const data: IDataObject = {
+						"message": `${message}`,
+							"tpoa": "Sender",
+								"recipient": [
+									{
+										"msisdn": `${to}`,
+									},
+							 ],
+							"scheduled":`${dateCadenaResult}`,
 					};
 					Object.assign(data);
 					const options: OptionsWithUri = {
